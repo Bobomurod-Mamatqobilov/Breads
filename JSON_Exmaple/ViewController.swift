9bdc86bb865e9dfcf6bbfbc20183111977968e4f
Mirsaidov Bekzod <mirsaidovB406@gmail.com>
2024-07-24 02:23:51 +0500

    Initial Commit

---
+++ JSON_Exmaple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON_Exmaple
+//
+//  Created by Mamatqobilov Bobomurod on 24/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
